3b421a40f82dee7698ccd12392f7476af2df9142
chasegarsee <chasegarsee@gmail.com>
2020-01-03 06:44:37 -0600

    building components out

--- src/components/CityPage/CityPage.jsx
+++ src/components/CityPage/CityPage.jsx
@@ -1,25 +1,49 @@
-import React, { useEffect, useContext, useState} from 'react'
-import { axiosWithAuth } from '../../utils/axiosWithAuth'
+import React, { useEffect, useContext, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { Button } from "semantic-ui-react";
+import { axiosWithAuth } from "../../utils/axiosWithAuth";
+import "./CityPage.scss";
+import nashville from "../assets/Nashville_sunset.jpg";
+import sanFrancisco from "../assets/sanfrancisco_sunset.jpg";
+import austin from "../assets/Austin_sunset.jpg";
+
+const cities = ["", nashville, sanFrancisco, austin];
 
 const CityPage = props => {
-    const [cityPackages, setCityPackages] = useState([])
-useEffect(()=>{
-    const city_id = props.match.params.id
+  const [cityPackages, setCityPackages] = useState([]);
+  useEffect(() => {
+    const city_id = props.match.params.id;
+
     //Make Axios call with city_id to retrieve the packages per city
     axiosWithAuth()
-    .get(`packages/city/${city_id}`)
-    .then (res => {
-        setCityPackages(res.data)
-        console.log(res.data)
-    })
-    .catch(err => console.log(err.res))
+      .get(`packages/city/${city_id}`)
+      .then(res => {
+        setCityPackages(res.data);
+        console.log(res.data);
+      })
+      .catch(err => console.log(err.res));
+  }, [props.match.params.id]);
 
-},[props.match.params.id])
-    
-    return ( 
-        <div className='city-wrapper'>            
+  console.log(cityPackages);
+
+  return (
+    <div className="city-wrapper">
+      {cityPackages.map(p => (
+        <div key={p.package_id} className="city-packages">
+          <h1>{p.name}</h1>
+          <p>{p.description}</p>
+          <p>Package Type: {p.type}</p>
+          <p>{p.price}</p>
+          <Button size="massive" color="blue">
+            <Link className="button-text" to={`/packages/${p.name}`}>
+              View Package
+            </Link>
+          </Button>
         </div>
-     );
-}
- 
+      ))}
+    </div>
+  );
+};
+
 export default CityPage;
--- src/components/Packages/Packages.jsx
+++ src/components/Packages/Packages.jsx
@@ -3,102 +3,12 @@ import React from "react";
 import "./Packages.scss";
 import { withRouter } from "react-router-dom";
 
-const Packages = () => {
+const Packages = props => {
+  console.log(props);
   return (
     <>
       <div>
         <h1>Package Name</h1>
-      </div>
-      <div className="suggestions-container">
-        <div className="suggestion">
-          <div className="category-header">
-            <h2>Food</h2>
-          </div>
-          <div className="category-container">
-            <div className="category-suggestion">
-              <div className="top-half">
-                <h3>Butcher Town Hall</h3>
-              </div>
-
-              <div className="bottom-half">
-                <a href="https://www.butchertownhall.com">
-                  butchertownhall.com
-                </a>
-                <p>
-                  Restaurant Description. This Could be very long so maybe try
-                  and write an extremely painfully long run on sentence to try
-                  and emulte the feeling of a long sentence and maybe we will
-                  get an idea of how long the sentence or description might be.
-                </p>
-                <a href="https://www.google.com/maps/dir//butcher+town+hall/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x88646703ae8d5811:0xe6537aa47c217248?sa=X&ved=2ahUKEwj3ltup8ebmAhVGCc0KHYePDWUQ9RcwDHoECAsQEA">
-                  Address: 1416 4th Ave N, Nashville, TN 37208
-                </a>
-              </div>
-            </div>
-            <div className="category-suggestion">
-              <h3>Butcher Town Hall</h3>
-              <p>
-                Restaurant Description. This Could be very long so maybe try and
-                write an extremely painfully long run on sentence to try and
-                emulte the feeling of a long sentence and maybe we will get an
-                idea of how long the sentence or description might be.
-              </p>
-              <a href="https://www.butchertownhall.com">www.website.com</a>
-              <a href="https://www.google.com/maps/dir//butcher+town+hall/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x88646703ae8d5811:0xe6537aa47c217248?sa=X&ved=2ahUKEwj3ltup8ebmAhVGCc0KHYePDWUQ9RcwDHoECAsQEA">
-                Address: 1416 4th Ave N, Nashville, TN 37208
-              </a>
-            </div>
-            <div className="category-suggestion">
-              <h3>Butcher Town Hall</h3>
-              <p>
-                Restaurant Description. This Could be very long so maybe try and
-                write an extremely painfully long run on sentence to try and
-                emulte the feeling of a long sentence and maybe we will get an
-                idea of how long the sentence or description might be.
-              </p>
-              <a href="https://www.butchertownhall.com">www.website.com</a>
-              <a href="https://www.google.com/maps/dir//butcher+town+hall/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x88646703ae8d5811:0xe6537aa47c217248?sa=X&ved=2ahUKEwj3ltup8ebmAhVGCc0KHYePDWUQ9RcwDHoECAsQEA">
-                Address: 1416 4th Ave N, Nashville, TN 37208
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="suggestion">
-          <div>
-            <h2>Attractions</h2>
-          </div>
-          <div className="category-container">
-            <div className="category-suggestion">
-              <h3>Butcher Town Hall</h3>
-              <p>
-                Restaurant Description. This Could be very long so maybe try and
-                write an extremely painfully long run on sentence to try and
-                emulte the feeling of a long sentence and maybe we will get an
-                idea of how long the sentence or description might be.
-              </p>
-              <a href="https://www.butchertownhall.com">www.website.com</a>
-              <a href="https://www.google.com/maps/dir//butcher+town+hall/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x88646703ae8d5811:0xe6537aa47c217248?sa=X&ved=2ahUKEwj3ltup8ebmAhVGCc0KHYePDWUQ9RcwDHoECAsQEA">
-                Address: 1416 4th Ave N, Nashville, TN 37208
-              </a>
-            </div>
-            <div className="category-suggestion">
-              <h3>Butcher Town Hall</h3>
-              <p>
-                Restaurant Description. This Could be very long so maybe try and
-                write an extremely painfully long run on sentence to try and
-                emulte the feeling of a long sentence and maybe we will get an
-                idea of how long the sentence or description might be.
-              </p>
-              <a href="https://www.butchertownhall.com">www.website.com</a>
-              <a href="https://www.google.com/maps/dir//butcher+town+hall/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x88646703ae8d5811:0xe6537aa47c217248?sa=X&ved=2ahUKEwj3ltup8ebmAhVGCc0KHYePDWUQ9RcwDHoECAsQEA">
-                Address: 1416 4th Ave N, Nashville, TN 37208
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="suggestion">
-          <h2>Entertainment</h2>
-        </div>
       </div>
     </>
   );
